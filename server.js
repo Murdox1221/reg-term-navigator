7935b3aa0e686c0bb9f554ffe65073d120f07a6c
Murdox1221 <golladayda@gmail.com>
2026-03-24 22:50:26 -0500

    Update

--- server.js
+++ server.js
@@ -1,29 +1,23 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-
-const app = express();
-const PORT = process.env.PORT || 3000;
+const express=require('express');
+const fs=require('fs');
+const path=require('path');
+const app=express();
+const PORT=process.env.PORT||3000;
 
 app.use(express.json());
 app.use(express.static(__dirname));
 
-const FILE = './data.json';
+const FILE='./data.json';
 
-app.get('/api/data', (req, res) => {
-  const data = JSON.parse(fs.readFileSync(FILE));
-  res.json(data);
+app.get('/api/data',(req,res)=>{
+ res.json(JSON.parse(fs.readFileSync(FILE)));
 });
 
-app.post('/api/terms', (req, res) => {
-  const data = JSON.parse(fs.readFileSync(FILE));
-  data.terms.push(req.body);
-  fs.writeFileSync(FILE, JSON.stringify(data, null, 2));
-  res.sendStatus(200);
+app.post('/api/practices',(req,res)=>{
+ const d=JSON.parse(fs.readFileSync(FILE));
+ d.practices=req.body;
+ fs.writeFileSync(FILE,JSON.stringify(d,null,2));
+ res.sendStatus(200);
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT,()=>console.log('running '+PORT));
